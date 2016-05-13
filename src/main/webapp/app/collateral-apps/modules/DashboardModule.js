3e1f7fa8b961abcafe3f248b6b23ea7ab4130e5e
Wilmary Laclé <wilmary.lacle@gmail.com>
2016-05-13 11:22:44 -0400

    Añadiendo select multiple para holidays en legal entity

--- src/main/webapp/app/collateral-apps/modules/DashboardModule.js
+++ src/main/webapp/app/collateral-apps/modules/DashboardModule.js
@@ -4,17 +4,17 @@
 
 /* Collateral App */
 var DashboardApp = angular.module("DashboardApp", [
-    "CollateralApp","datatables"
+    "CollateralApp"
 ]);
 
 /* Setup Rounting For All Pages */
-DashboardApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+DashboardApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     // Redirect any unmatched url
     $urlRouterProvider.otherwise("home");
 
     $stateProvider
 
-        .state('home.config', {
+        .state('home.config',{
             abstract: true,
             url: '/config'
         })
@@ -23,12 +23,13 @@ DashboardApp.config(['$stateProvider', '$urlRouterProvider', function ($statePro
             data: {pageTitle: 'Legal Entity'},
 
             views: {
-                'content@home': {
+                'content@home':
+                {
                     templateUrl: paths.views + "/configuration/legal_entity.html",
                 }
             },
             resolve: {
-                deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
                     return $ocLazyLoad.load({
                         name: 'Collateral App',
                         insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
@@ -36,35 +37,23 @@ DashboardApp.config(['$stateProvider', '$urlRouterProvider', function ($statePro
 
                             /* select css*/
                             'assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css',
-                            'assets/vendor/select2/dist/css/select2.min.css',
-                            'assets/vendor/select2-bootstrap-css/select2-bootstrap.min.css',
                             'assets/vendor/angular-ui-select/dist/select.min.css',
+                            'assets/vendor/multiselect/css/multi-select.css',
 
                             /* form css */
-                            'assets/vendor/bootstrap-fileinput/css/fileinput.min.css',
-                            /*'assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
-                             'assets/global/plugins/bootstrap-markdown/css/bootstrap-markdown.min.css',*/
+                             'assets/vendor/bootstrap-fileinput/css/fileinput.min.css',
 
                             /* select js*/
                             'assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js',
                             'assets/vendor/select2/dist/js/select2.full.min.js',
-
-                            'assets/pages/scripts/components-bootstrap-select.min.js',
-                            'assets/pages/scripts/components-select2.min.js',
                             'assets/vendor/angular-ui-select/dist/select.min.js',
-
+                            'assets/vendor/multiselect/js/jquery.multi-select.js',
+                            'assets/pages/scripts/components-multi-select.min.js',
 
                             /* form js */
                             'assets/vendor/bootstrap-fileinput/js/fileinput.min.js',
-                            /*'assets/global/plugins/fuelux/js/spinner.min.js',
-                             'assets/global/plugins/jquery-inputmask/jquery.inputmask.bundle.min.js',
-                             'assets/global/plugins/jquery.input-ip-address-control-1.0.min.js',
-                             'assets/global/plugins/bootstrap-pwstrength/pwstrength-bootstrap.min.js',
-                             'assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
-                             'assets/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js',
-                             'assets/global/plugins/bootstrap-touchspin/bootstrap.touchspin.js',
-                             'assets/pages/scripts/components-form-tools-2.js'*/
 
+                            /* Controller */
                             'collateral-apps/controllers/configuration/LegalEntityController.js'
 
                         ]
