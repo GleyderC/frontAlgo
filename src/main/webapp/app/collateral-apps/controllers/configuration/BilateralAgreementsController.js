c8a464a5de2aca276a347e0976fe58ff90b4b405
Guillmar Ortiz <guillmar.ortiz@gmail.com>
2016-05-23 23:07:21 -0400

    Se corrigieron detalles en la interfaz de Contratos bilaterales y se realizaron mejoras en el manejo de tabs en general

--- src/main/webapp/app/collateral-apps/controllers/configuration/BilateralAgreementsController.js
+++ src/main/webapp/app/collateral-apps/controllers/configuration/BilateralAgreementsController.js
@@ -27,8 +27,17 @@ DashboardApp.controller(
 
             $scope.workspaceTabs = {
                 name: 'collateral-tabs',
-                active: 0,
-                tabList: []
+                active: true,
+                tabList: [
+                    {
+                        head: {
+                            icon: 'fa fa-money',
+                            text: 'Search Billateral Agreement'
+                        },
+                        templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilateral_a_add_search.html",
+                        active: true
+                    }
+                ]
             };
 
             $scope.addNewBillateralAgreement = function () {
@@ -37,14 +46,14 @@ DashboardApp.controller(
                     {
                         head: {
                             icon: '',
-                            text: 'New Billateral Agreement(' + ($scope.workspaceTabs.tabList.length + 1) + ')'
+                            text: 'New Billateral Agreement(' + ($scope.workspaceTabs.tabList.length) + ')'
                         },
-                        templateUrl: paths.views + "/configuration/BilateralAgreements/index.html",
+                        templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilteral_a_tabs_container.html",
                         closable: true
                     }
                 );
 
-                var offset = $("#container-tabs-bilateral-contract").offset().top;
+                //var offset = $("#container-tabs-bilateral-contract").offset().top;
                 //elementService.scrollToElement("container-tabs-bilateral-contract", offset);
             };
 
@@ -54,14 +63,14 @@ DashboardApp.controller(
                     {
                         head: {
                             icon: '',
-                            text: 'Editing Billateral Agreement (' + ($scope.workspaceTabs.tabList.length + 1) + ')'
+                            text: 'Editing Billateral Agreement (' + ($scope.workspaceTabs.tabList.length) + ')'
                         },
-                        templateUrl: paths.views + "/configuration/BilateralAgreements/index.html",
+                        templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilteral_a_tabs_container.html",
                         closable: true
                     }
                 );
 
-                var offset = $("#container-tabs-bilateral-contract").offset().top;
+                //var offset = $("#container-tabs-bilateral-contract").offset().top;
                 //elementService.scrollToElement("container-tabs-bilateral-contract", offset);
 
             };
@@ -96,28 +105,28 @@ DashboardApp.controller(
                 tabList: [
                     {
                         head: {
-                            icon: 'glyphicon-blackboard',
+                            icon: 'glyphicon glyphicon-blackboard',
                             text: 'Main'
                         },
                         templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilateral_a_main.html"
                     },
                     {
                         head: {
-                            icon: 'glyphicon-list-alt',
+                            icon: 'glyphicon glyphicon-list-alt',
                             text: 'CSA Margin'
                         },
                         templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilateral_a_csa_margins.html"
                     },
                     {
                         head: {
-                            icon: 'glyphicons-piggy-bank',
+                            icon: 'glyphicon glyphicon-piggy-bank',
                             text: 'Eligible currencies'
                         },
                         templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilateral_a_eligible_currencies.html"
                     },
                     {
                         head: {
-                            icon: 'glyphicon-eye-open',
+                            icon: 'glyphicon glyphicon-eye-open',
                             text: 'Eligible securities'
                         },
                         templateUrl: paths.views + "/configuration/BilateralAgreements/le_bilateral_a_eligible_securities.html"
@@ -126,6 +135,81 @@ DashboardApp.controller(
             };
 
         }]);
+
+DashboardApp.controller('LEBillateralAgrSearchController', ['$scope', '$request', '$interval', function ($scope, $request, $interval) {
+
+    //FIRST SEARCH TAB
+
+    $scope.editRow = function (grid, row) {
+        console.log("editing contract")
+        $scope.editBillateralAgreement();
+    }
+
+    $scope.deleteRow = function (grid, row) {
+        console.log("deleting")
+    }
+
+    $scope.gridOptions = {
+        columnDefs: [
+            {
+                field: 'contract_name',
+                name: 'Contract name'
+            },
+            {
+                field: 'contract_type',
+                name: 'Contract type'
+            },
+            {
+                name: 'Actions',
+                cellTemplate: paths.tpls + '/ActionsButtonsTpl.html',
+                enableColumnMenu: false,
+                width: 160
+            }
+        ],
+        rowHeight: 35, // set height to each row
+        enableGridMenu: true,
+        exporterCsvFilename: 'Bilateral_Agreements.csv',
+        exporterPdfDefaultStyle: {fontSize: 9},
+        exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+        exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+        exporterPdfHeader: {text: "Bilateral Agreements", style: 'headerStyle'},
+        exporterPdfFooter: function (currentPage, pageCount) {
+            return {text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle'};
+        },
+        exporterPdfCustomFormatter: function (docDefinition) {
+            docDefinition.styles.headerStyle = {fontSize: 22, bold: true};
+            docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
+            return docDefinition;
+        },
+        exporterPdfOrientation: 'portrait',
+        exporterPdfPageSize: 'LETTER',
+        exporterPdfMaxGridWidth: 500,
+        exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+        onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+            // call resize every 500 ms for 5 s after modal finishes opening
+            $interval( function() {
+                $scope.gridApi.core.handleWindowResize();
+            }, 1000, 10);
+        }
+    };
+
+    $scope.gridOptions.data = [
+        {
+            contract_name: "CSA Contract Swaps",
+            contract_type: "CSA"
+        },
+        {
+            contract_name: "CSA Contract FX Swaps",
+            contract_type: "CSA"
+        },
+        {
+            contract_name: "SCSA Contract Rates Swaps",
+            contract_type: "SCSA"
+        }
+    ];
+
+}]);
 
 DashboardApp.controller('LEBillateralAgrEligibleCurrenciesController', ['$scope', '$request', '$interval', function ($scope, $request, $interval) {
 
@@ -140,16 +224,15 @@ DashboardApp.controller('LEBillateralAgrEligibleCurrenciesController', ['$scope'
     $scope.gridOptions = {
         columnDefs: [
             {name: 'Currency'},
-            {name: 'Type'},
             {name: 'Fixed Rate'},
             {name: 'Index'},
             {name: 'Tenor'},
-            {name: 'Source'},
+            /*{name: 'Source'},
             {name: 'Factor'},
             {name: 'Compound'},
             {name: 'Included'},
             {name: 'Projected'},
-            {name: 'Adjustment Currency'},
+            {name: 'Adjustment Currency'},*/
             {
                 name: 'Actions',
                 cellTemplate: paths.tpls + '/ActionsButtonsTpl.html',
