4c071b4452a0b612a991a1356323f15960ba089e
Wilmary Laclé <wilmary.lacle@gmail.com>
2016-05-28 23:34:51 -0400

    Creación de servicio para agregar y editar filas utilizando modal para ui-grid. El mismo fue implementado en contact ui-grid

---
+++ src/main/webapp/app/collateral-apps/services/RowEditorModalService.js
@@ -0,0 +1,26 @@
+angular.module('DashboardApp')
+    .factory('RowEditorService',[ '$uibModal', function ($uibModal) {
+    var service = {};
+    service.editRow = editRow;
+
+    function editRow(grid, row) {
+        //console.log(row);
+        $uibModal.open({
+            templateUrl: 'edit-modal-contact.html',
+            size: 'lg',
+            backdrop: false,
+            controller: ['$scope', '$uibModalInstance', 'grid', 'row', RowEditCtrl],
+            controllerAs: 'vm',
+            resolve: {
+                grid: function () {
+                    return grid;
+                },
+                row: function () {
+                    return row;
+                }
+            }
+        });
+
+    }
+    return service;
+}]);
