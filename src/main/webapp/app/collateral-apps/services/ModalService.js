f430ac2eeff1dbe9f6216d47913bdad4cfea3f8b
Guillmar Ortiz <guillmar.ortiz@gmail.com>
2016-05-31 16:32:22 -0430

    Culminado el servicio para la carga de modales y se corrigieron fallos de visualización de la interfaz

--- src/main/webapp/app/collateral-apps/services/ModalService.js
+++ src/main/webapp/app/collateral-apps/services/ModalService.js
@@ -1,7 +1,50 @@
 angular.module('DashboardApp')
-    .factory('ModalService',[ '$uibModal', function ($uibModal) {
+    .service('ModalService', ['$uibModal', '$log', function ($uibModal, $log) {
 
-        return function (){
-        }
+        var ModalService = {};
+        var modalInstance = null;
 
-}]);
+        ModalService.animationsEnabled = true;
+
+        ModalService.open = function (size) {
+
+            modalInstance = $uibModal.open({
+                animation: ModalService.animationsEnabled,
+                templateUrl: paths.tpls + '/ModalTpl.html',
+                controller: function ($scope, $uibModalInstance) {
+                    
+                    $scope.ok = function () {
+                        $uibModalInstance.close($scope.selected.item);
+                    };
+
+                    $scope.cancel = function () {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+
+                },
+                size: size,
+                resolve: {
+                    items: function () {
+                        return [];
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                ModalService.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        ModalService.close = function () {
+            modalInstance.dismiss('cancel');
+        };
+
+        ModalService.toggleAnimation = function () {
+            ModalService.animationsEnabled = !ModalService.animationsEnabled;
+        };
+
+        return ModalService;
+
+    }]);
