910e82e757f6555fbbeaf095e7f54431bbcb09f1
Guillmar Ortiz <guillmar.ortiz@gmail.com>
2016-06-21 16:44:45 -0430

    Continuando desarrollo tabs

--- src/main/webapp/app/collateral-apps/directives/MenuCollateral.js
+++ src/main/webapp/app/collateral-apps/directives/MenuCollateral.js
@@ -3,28 +3,180 @@ DashboardApp.directive('menuCollateral', ['$rootScope', function ($rootScope) {
     return {
         restrict: "AE",
         templateUrl: "collateral-apps/tpl/MenuCollateral.html",
-        link: function(scope, iElem, iAttrs){
-            
+        link: function (scope, iElem, iAttrs) {
+
         }
     }
 }]);
 
-DashboardApp.service('MenuService', [function(){
+DashboardApp.service('MenuService', function () {
+    /*this.MenuTree = [
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: 'GrandPa'
+     },
+     content: '',
+     templateUrl: '',
+     childrenTabs: [
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: 'Child1'
+     },
+     content: '',
+     templateUrl: '',
+     childrenTabs: [
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: 'last child1'
+     },
+     content: '',
+     templateUrl: '',
+     childrenTabs: [
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: 'the last of us'
+     },
+     content: '',
+     templateUrl: '',
+     childrenTabs: [],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     }
+     ],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     }
+     ],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     },
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: ''
+     },
+     content: '',
+     templateUrl: 'child2',
+     childrenTabs: [
+     {
+     head: {
+     icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+     text: 'last child2'
+     },
+     content: '',
+     templateUrl: '',
+     childrenTabs: [],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     }
+     ],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     }
+     ],
+     resolve: {
+     formData: []
+     },
+     disabled: false,
+     closable: false,
+     callback: function () {
+     }
+     }
+     ]*/
+
     this.MenuTree = [
         {
             head: {
                 icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
-                text: ''
+                text: 'GrandPa'
             },
-            content: '',
+            content: 'Content GrandPa',
             templateUrl: '',
             childrenTabs: [
                 {
                     head: {
                         icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
-                        text: ''
+                        text: 'Father 1'
                     },
-                    content: '',
+                    content: 'Content Father1',
+                    templateUrl: '',
+                    childrenTabs: [
+                        {
+                            head: {
+                                icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+                                text: 'Son 1'
+                            },
+                            content: 'Content Son 1',
+                            templateUrl: '',
+                            childrenTabs: [],
+                            resolve: {
+                                formData: []
+                            },
+                            disabled: false,
+                            closable: false,
+                            callback: function () {
+                            }
+                        },
+                        {
+                            head: {
+                                icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+                                text: 'Son 2'
+                            },
+                            content: 'Content Son 2',
+                            templateUrl: '',
+                            childrenTabs: [],
+                            resolve: {
+                                formData: []
+                            },
+                            disabled: false,
+                            closable: false,
+                            callback: function () {
+                            }
+                        }
+                    ],
+                    resolve: {
+                        formData: []
+                    },
+                    disabled: false,
+                    closable: false,
+                    callback: function () {
+                    }
+                }
+                , {
+                    head: {
+                        icon: 'glyphicon glyphicon-refresh glyphicon-refresh-animate',
+                        text: 'Father 2'
+                    },
+                    content: 'Content Father2',
                     templateUrl: '',
                     childrenTabs: [],
                     resolve: {
@@ -45,4 +197,4 @@ DashboardApp.service('MenuService', [function(){
             }
         }
     ]
-}]);
+});
