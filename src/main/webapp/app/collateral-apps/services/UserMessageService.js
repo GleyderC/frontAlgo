aec16e60a540adf51a3ef8a5556704151642a3a0
Gleyder Cova <gleyder4@gmail.com>
2016-06-23 02:08:10 -0430

    Agregada la funcion de conexion al servicio UserMessage/Insert, para crear un Mensaje nuevo

--- src/main/webapp/app/collateral-apps/services/UserMessageService.js
+++ src/main/webapp/app/collateral-apps/services/UserMessageService.js
@@ -10,8 +10,16 @@ angular.module('DashboardApp')
             			date :date
             	};
                 return $request.get('/servlet/UserMessages/SelectByDate',param);
-      };
-        
+        };
+        this.create = function(data){
+        	var params = data; 
+//        	{ "hasBeenRead" : false , 
+//        		"messageContentBasic" : "Basic Content Message 123 ",
+//        		"messageContentExtendes": "Extended Content Message 3232",
+//        		"hasBeenSentByEmail":false, 
+//        		"UserMessageType":"INTEREST_STATEMENT_CORRECTED"}
+            return $request.post('/servlet/UserMessages/Insert',params);
+        }
       
     
   }]);
