4e719b33177bf22f39e0dad28bced8b4da2de1d1
Gleyder Cova <gleyder4@gmail.com>
2016-07-21 01:09:02 -0400

    Agregando difference y difference %  en dispute despues de obtener el calculo

--- src/main/webapp/app/collateral-apps/controllers/collateral/margin_call/MarginCallDetailController.js
+++ src/main/webapp/app/collateral-apps/controllers/collateral/margin_call/MarginCallDetailController.js
@@ -2,8 +2,8 @@
 
 var DashboardApp = angular.module('DashboardApp');
 
-DashboardApp.controller('MarginCallDetailController', ['$scope','localStorageService', 'uiGridConstants', 'MarginCallService','$timeout',
-    function ($scope, localStorage,uiGridConstants, MarginCallService, $timeout) {
+DashboardApp.controller('MarginCallDetailController', ['$scope','localStorageService', 'uiGridConstants', 'MarginCallService','$timeout', 'toastr',
+    function ($scope, localStorage,uiGridConstants, MarginCallService, $timeout,$toastr) {
 
         $scope.sendFlag = false;
         $scope.currentMarginCall = $scope.parameters;
@@ -73,98 +73,113 @@ DashboardApp.controller('MarginCallDetailController', ['$scope','localStorageSer
             }
             
             $scope.tolerance = $scope.MarginCallDetail.marginCall.marginCallElementsByLiabilityType.CSA.marginCallCalculations.csaDisputesCalculations.tolerance;
+            $scope.myValue = $scope.MarginCallDetail.marginCall.marginCallElementsByLiabilityType.CSA.marginCallCalculations.csaDisputesCalculations.myValue;
+            $scope.disputeStatus = $scope.MarginCallDetail.marginCall.marginCallElementsByLiabilityType.CSA.marginCallCalculations.csaDisputesCalculations.disputeStatusEnum;
+        	
             $scope.dispute = {
+            		contractId    : $scope.MarginCallDetail.contract.internalId,
             		margincallId  : $scope.currentMarginCall.marginCalls[0].id,
-            		tolerance :  $scope.tolerance,
-            		agreedMargin : 0,
-            		disputeComments : ""
+            		disputeCalculations  : {
+            			counterpartyValue : 0,
+            			difference 		: 0 ,
+            			differencePercentage 		: 0 ,
+            			disputeStatus : $scope.disputeStatus, 
+            			tolerance 	  : $scope.tolerance,
+            			myValue   	  : $scope.myValue ,
+            			disputeComments : "",
+            			agreedMargin : 0
+            		}
+            		
+            		
             }; 
-            $scope.$watchCollection("dispute",function(n,o){
+            $scope.$watchCollection("dispute.disputeCalculations",function(n,o){
             	if(_.isEqual(n,o)){
             		return false; 
             	}
             	$scope.disputeEdit = true ;
             });
 
-            $scope.tabs1 = [
-                            {
-                                id: 'mc-csa-margin',
-                                title: 'CSA Margins',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_csa_margin.html',
-                                icon: 'icon-call-in'
-                            },
-                            {
-                                id: 'mc-csa-allocations',
-                                title: 'CSA Allocations',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_csa_allocations.html',
-                                icon: ''	
-                            },
-                            {
-                                id: 'mc-im-collateral-allocations',
-                                title: 'IM Collateral Allocations',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_im_collateral_allocations.html',
-                                icon: ''
-                            },
-                            {
-                                id: 'mc-vm-collateral-allocations',
-                                title: 'VM Collateral Allocations',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_vm_collateral_allocations.html',
-                                icon: ''
-                            },
-                            {
-                                id: 'mc-collateral-substitution',
-                                title: 'Collateral Substitution',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_substitution.html',
-                                icon: ''
-                            },
-                            {
-                                id: 'messaging-repository',
-                                title: 'Messaging Repository',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_messaging_repository.html',
-                                icon: ''
-                            }
-                        ];
-                 
-                        $scope.tabs2 = [
-                            {
-                                id: 'mc-trades',
-                                title: 'Trades (underlyings)',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_trades.html',
-                                icon: '',
-                                active : true,
-                                disabled : false
-                            },
-                            {
-                                id: 'mc-collateral-inventory',
-                                title: 'Position',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_inventory.html',
-                                icon: '',
-                                active : true
-                                
-                            },
-                            {
-                                id: 'mc-collateral-pool',
-                                title: 'Pool',
-                                templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_pool.html',
-                                icon: '',
-                                	active : true
-                            }
-
-                        ];
-            
         });
+
+        $scope.tabs1 = [
+                        {
+                            id: 'mc-csa-margin',
+                            title: 'CSA Margins',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_csa_margin.html',
+                            icon: 'icon-call-in'
+                        },
+                        {
+                            id: 'mc-csa-allocations',
+                            title: 'CSA Allocations',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_csa_allocations.html',
+                            icon: ''	
+                        },
+                        {
+                            id: 'mc-im-collateral-allocations',
+                            title: 'IM Collateral Allocations',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_im_collateral_allocations.html',
+                            icon: ''
+                        },
+                        {
+                            id: 'mc-vm-collateral-allocations',
+                            title: 'VM Collateral Allocations',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_vm_collateral_allocations.html',
+                            icon: ''
+                        },
+                        {
+                            id: 'mc-collateral-substitution',
+                            title: 'Collateral Substitution',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_substitution.html',
+                            icon: ''
+                        },
+                        {
+                            id: 'messaging-repository',
+                            title: 'Messaging Repository',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_messaging_repository.html',
+                            icon: ''
+                        }
+                    ];
+             
+                    $scope.tabs2 = [
+                        {
+                            id: 'mc-trades',
+                            title: 'Trades (underlyings)',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_trades.html',
+                            icon: '',
+                            active : true,
+                            disabled : false
+                        },
+                        {
+                            id: 'mc-collateral-inventory',
+                            title: 'Position',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_inventory.html',
+                            icon: '',
+                            active : true
+                            
+                        },
+                        {
+                            id: 'mc-collateral-pool',
+                            title: 'Pool',
+                            templateUrl: 'collateral-apps/views/collateral/margin_call/mc_collateral_pool.html',
+                            icon: '',
+                            	active : true
+                        }
+
+                    ];
+        
         $scope.updateDispute     = function(dispute){
         	MarginCallService.updateDispute(dispute).success(function(resp){
-        		console.log(resp);
+        		$scope.dispute.disputeCalculations.disputeStatus   = resp.dataResponse.disputeCalculations.disputeStatusEnum;
+        		$scope.dispute.disputeCalculations.difference   = resp.dataResponse.disputeCalculations.difference;
+        		$scope.dispute.disputeCalculations.differencePercentage   = resp.dataResponse.disputeCalculations.differencePercentage;
+        		$toastr.success("Dispute updated successfully","Update dispute data",{closeButton: true});
         	});
         };
       
         this.sendMargin = function () {
             $scope.sendFlag = true;
-            //console.log($scope.sendFlag);
             var result = MarginCallService.sendIssueMarginCall($scope.MarginCallDetail.marginCall.id, "CSA").
                 then(function (result) {
-                    //console.log(result);g
                     $scope.sendFlag = false;
             },
                 function (error) {
