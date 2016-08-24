75ab1562a812cd8d80bdd6c14f0a7c9adc918b16
Wilmary Laclé <wilmary.lacle@gmail.com>
2016-08-24 00:02:20 -0400

    Añadiendo listado de grupos y usuarios en gestion de usuarios

--- src/main/webapp/app/collateral-apps/controllers/DashboardController.js
+++ src/main/webapp/app/collateral-apps/controllers/DashboardController.js
@@ -83,6 +83,95 @@ angular.module('CollateralApp').controller('DashboardController',
 							},
 							data: []
 						}
+						this.gridGroupsOptions = {
+
+							columnDefs: [
+								{
+									field: 'name',
+									name: 'Name'
+								},
+								{
+									field: 'description',
+									name: 'Description'
+								},
+								{
+									name: 'Actions',
+									cellTemplate: paths.tpls + '/ActionsButtonsTpl.html',
+									enableColumnMenu: false,
+									enableCellEdit: false,
+									width: 120,
+									enableFiltering: false
+								}
+							],
+							enableGridMenu: true,
+							enableFiltering: true,
+							exporterCsvFilename: 'groups.csv',
+							exporterPdfDefaultStyle: {fontSize: 9},
+							exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+							exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+							exporterPdfHeader: {text: "Groups", style: 'headerStyle'},
+							exporterPdfFooter: function (currentPage, pageCount) {
+								return {text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle'};
+							},
+							exporterPdfCustomFormatter: function (docDefinition) {
+								docDefinition.styles.headerStyle = {fontSize: 22, bold: true};
+								docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
+								return docDefinition;
+							},
+							exporterPdfOrientation: 'portrait',
+							exporterPdfPageSize: 'LETTER',
+							exporterPdfMaxGridWidth: 500,
+							exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+							onRegisterApi: function (gridApi) {
+								$scope.gridApi = gridApi;
+							},
+							data: [{name:"test",description:""}]
+						}
+						this.gridUsersOptions = {
+
+							columnDefs: [
+								{
+									field: 'name',
+								},
+								{
+									field: 'lastname',
+								},
+								{
+									field: 'email',
+								},
+								{
+									name: 'Actions',
+									cellTemplate: paths.tpls + '/ActionsButtonsTpl.html',
+									enableColumnMenu: false,
+									enableCellEdit: false,
+									width: 120,
+									enableFiltering: false
+								}
+							],
+							enableGridMenu: true,
+							enableFiltering: true,
+							exporterCsvFilename: 'users.csv',
+							exporterPdfDefaultStyle: {fontSize: 9},
+							exporterPdfTableStyle: {margin: [30, 30, 30, 30]},
+							exporterPdfTableHeaderStyle: {fontSize: 10, bold: true, italics: true, color: 'red'},
+							exporterPdfHeader: {text: "users", style: 'headerStyle'},
+							exporterPdfFooter: function (currentPage, pageCount) {
+								return {text: currentPage.toString() + ' of ' + pageCount.toString(), style: 'footerStyle'};
+							},
+							exporterPdfCustomFormatter: function (docDefinition) {
+								docDefinition.styles.headerStyle = {fontSize: 22, bold: true};
+								docDefinition.styles.footerStyle = {fontSize: 10, bold: true};
+								return docDefinition;
+							},
+							exporterPdfOrientation: 'portrait',
+							exporterPdfPageSize: 'LETTER',
+							exporterPdfMaxGridWidth: 500,
+							exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+							onRegisterApi: function (gridApi) {
+								$scope.gridApi = gridApi;
+							},
+							data: [{name:"Daniel",lastname:"Nebrera",email:"danielnebrera@commonsms.com"}]
+						}
 
 						//data DEMO
 						this.gridOptions.data.push(
