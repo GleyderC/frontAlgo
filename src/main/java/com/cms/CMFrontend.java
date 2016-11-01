28066794a45badd7981b8246c6489f9e23af3d88
Guillmar Ortiz <guillmar.ortiz@gmail.com>
2016-10-31 22:44:18 -0300

    Corrigiendo fallos en el despliegue

--- src/main/java/com/cms/CMFrontend.java
+++ src/main/java/com/cms/CMFrontend.java
@@ -2,14 +2,24 @@ package com.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CMFrontend {
+public class CMFrontend extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(CMFrontend.class, args);
 
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class we actually don't
+        // need to override this method.
+        return application;
     }
 
 }
